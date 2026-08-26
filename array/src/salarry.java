44ea67ab2484deaf8848bb292553e57f3ec4e20a
manthan142005 <manthannayak1234567@gmail.com>
2026-08-26 11:55:08 +0530

    first commit

---
+++ array/src/salarry.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double average(int[] salary) {
+        int sum = 0;
+        int min = salary[0];
+        int max = salary[0];
+
+        for (int s : salary) {
+            sum += s;
+            min = Math.min(min, s);
+            max = Math.max(max, s);
+        }
+
+        sum = sum - min - max;
+
+        return (double) sum / (salary.length - 2);
+    }
+}
